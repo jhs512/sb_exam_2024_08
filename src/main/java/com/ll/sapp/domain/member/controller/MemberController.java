de3006ec66cdca6d82e8c977bdbe3cd33461a971
jhs512 <jangka512@gmail.com>
2024-08-06 10:20:16 +0900

    work

---
+++ src/main/java/com/ll/sapp/domain/member/controller/MemberController.java
@@ -0,0 +1,14 @@
+package com.ll.sapp.domain.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+    @GetMapping("/login")
+    public String showLogin() {
+        return "member/login";
+    }
+}
